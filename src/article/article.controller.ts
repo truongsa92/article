6cf429b005852ae7c8d66a920eddc725d27211a5
treintt <trien.tran@ekoios.vn>
2021-01-25 16:50:21 +0700

    edit get article and comment

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,45 +1,58 @@
-import { Get, Post, Body, Query, Param, Controller, ParseIntPipe } from '@nestjs/common';
+import {
+  Get,
+  Post,
+  Body,
+  Query,
+  Param,
+  Controller,
+  ParseIntPipe,
+} from "@nestjs/common";
 import { ApiBody, ApiOperation, ApiProperty, ApiParam } from "@nestjs/swagger";
 
-import { ArticleService } from './article.service';
-import { CreateArticleDto, CreateCommentDto, FilterArticle } from './dto';
-import { ArticleEntity } from './article.entity';
-import { CommentEntity } from './comment.entity';
-import { ArticlesRO, ArticleRO, CommentsRO } from './article.interface'
+import { ArticleService } from "./article.service";
+import {
+  CreateArticleDto,
+  CreateCommentDto,
+  FilterArticle,
+  ArticleId,
+} from "./dto";
+import { ArticleEntity } from "./article.entity";
+import { CommentEntity } from "./comment.entity";
+import { ArticlesRO, ArticleRO, CommentsRO } from "./article.interface";
 
-@Controller('articles')
+@Controller("articles")
 export class ArticleController {
-
-  constructor(private readonly articleService: ArticleService) { }
+  constructor(private readonly articleService: ArticleService) {}
 
   @Get()
   async findAll(@Query() query: FilterArticle): Promise<ArticlesRO> {
     return await this.articleService.findAll(query);
   }
 
-  @Get(':id')
-  @ApiParam({name: 'id', type: Number, required: true })
-  async findOne(@Param('id', ParseIntPipe) id: number): Promise<ArticleRO> {
-    return await this.articleService.findOne({ id });
+  @Get(":id")
+  @ApiParam({ name: "id", type: ArticleId, required: true })
+  async findOne(@Param() id: ArticleId): Promise<ArticleRO> {
+    return await this.articleService.findOne(id);
   }
 
-  @Get(':articleId/comments')
-  @ApiParam({name: 'articleId', type: Number, required: true })
-  async getComments(@Param('articleId', ParseIntPipe) articleId): Promise<CommentsRO> {
-    return await this.articleService.getComments(articleId);
+  @Get(":id/comments")
+  @ApiParam({ name: "id", type: ArticleId, required: true })
+  async getComments(@Param() id: ArticleId): Promise<CommentsRO> {
+    return await this.articleService.getComments(id);
   }
 
   @Post()
-  @ApiOperation({operationId: 'createArticle'})
-  @ApiBody({type: CreateArticleDto, required: true})
+  @ApiOperation({ operationId: "createArticle" })
+  @ApiBody({ type: CreateArticleDto, required: true })
   async create(@Body() articleData: CreateArticleDto): Promise<ArticleEntity> {
     return this.articleService.create(articleData);
   }
 
-  @Post('comments')
-  @ApiOperation({operationId: 'createComment'})
-  async createComment(@Body() commentData: CreateCommentDto): Promise<CommentEntity> {
+  @Post("comments")
+  @ApiOperation({ operationId: "createComment" })
+  async createComment(
+    @Body() commentData: CreateCommentDto
+  ): Promise<CommentEntity> {
     return await this.articleService.createComment(commentData);
   }
-
 }
